aed9c7bb8523075a6ff2e1a93f34e97a7eedb332
Sabrina Fazio <sabrinafazio31@gmail.com>
2021-04-28 15:56:24 +0200

    solution

--- src/main/java/m02/s10/ex/Exercise1.java
+++ src/main/java/m02/s10/ex/Exercise1.java
@@ -1,10 +1,33 @@
 package m02.s10.ex;
 
 public class Exercise1 {
-    public static void main(String[] args) {
-        // TODO: print all values in [0 .. 9]
-        // (1) for loop
-        // (2) while loop
-        // (3) do-while loop
-    }
+	public static void main(String[] args) {
+		// TODO: print all values in [0 .. 9]
+
+		// (1) for loop
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("----");
+		
+		
+		// (2) while loop
+		int i= 0;
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+
+		}
+		
+		System.out.println("----");
+
+		// (3) do-while loop
+		int j= 0;
+		do {
+			System.out.println(j);
+			j++;
+		} while (j<10);
+	}
+
 }
